3d643184f05e2ad1e7f8a62dfb0220f5208d4c29
Xavier Ang <xaveang@gmail.com>
2025-03-27 21:51:56 +0800

    Enable checking adjacent discs in all directions

--- lib/fourx.ex
+++ lib/fourx.ex
@@ -8,34 +8,44 @@ defmodule Fourx do
       |> add_winning_positions
       |> Map.filter(fn {_k, v} -> v === player end)
 
-    find_adjacent(map, nx)
+    find_adjacent(map, nx, :horizontal)
   end
 
-  def find_adjacent(map, nx), do: find_adjacent(Map.keys(map), nx, 0, 1)
-  def find_adjacent(values, _nx, start, consecutive) when
+  def find_adjacent(map, nx, direction) do
+    diff = case direction do
+      :vertical -> nx
+      :horizontal -> 1
+      :diagonal_right -> nx + 1
+      :diagonal_left -> nx - 1
+    end
+
+    find_adjacent(Map.keys(map), diff, 0, 1)
+  end
+      
+  def find_adjacent(values, _diff, start_index, consecutive) when
     length(values) <= 1
-    or length(values) <= start + consecutive
+    or length(values) <= start_index + consecutive
   do
-    {start, consecutive}
+    {start_index, consecutive}
   end
 
-  def find_adjacent(values, nx, start_index, consecutive) do
+  def find_adjacent(values, diff, start_index, consecutive) do
     start = Enum.at(values, start_index)
     adj = Enum.find(values, fn x ->
-      next = start + consecutive * nx
-      IO.puts get_row(start, nx)
-      next === x and get_row(next, nx) - get_row(start, nx) === consecutive
+      next = start + consecutive * diff
+      next === x and get_row(next, diff) - get_row(start, diff) === consecutive
     end)
 
     cond do
       consecutive === 4 -> {start, consecutive}
-      adj !== nil -> find_adjacent(values, nx, start_index, consecutive + 1)
-      true -> find_adjacent(values, nx, start_index + 1, 1)
+      adj !== nil -> find_adjacent(values, diff, start_index, consecutive + 1)
+      true -> find_adjacent(values, diff, start_index + 1, 1)
     end
   end
 
-  defp get_row(x, nx) do
-    if x < 0 do 0 else 1 + get_row(x - nx, nx) end
+  # 1-indexed
+  defp get_row(x, diff) do
+    if x < 0 do 0 else 1 + get_row(x - diff, diff) end
   end
 
   def create_board(nx, ny) do
