78ca134b86745bc92964b47c0ff1f9d6f3bd9414
Xavier Ang <xaveang@gmail.com>
2025-03-27 11:13:36 +0800

    Add flat representation and winning position example

--- lib/fourx.ex
+++ lib/fourx.ex
@@ -2,13 +2,23 @@ defmodule Fourx do
   @type board_piece :: nil | 0 | 1
 
   def board do
-    map = create_board(7, 6)
-    map
+    _map = create_board(:flat, 7, 6)
+      |> add_winning_positions(:flat)
+      |> Map.filter(fn {_k, v} -> v !== nil end)
   end
 
   def create_board(nx, ny) do
     # Create nx no. of cols
     col = Enum.into(0..ny-1, %{}, &{&1, nil})
     Enum.into(0..nx-1, %{}, &{&1, col})
+  end
+
+  def create_board(:flat, nx, ny) do
+    len = nx * ny - 1
+    Map.new(0..len, &{&1, nil})
+  end
+
+  def add_winning_positions(map, :flat) do
+    %{ map | 0 => 0, 1 => 1, 2 => 0, 3 => 1, 4 => 0, 5 => 1, 6 => 0, 8 => 1, 9 => 0, 10 => 1, 11 => 0, 16 => 0, 17 => 1, 18 => 0, 24 => 1 }
   end
 end
